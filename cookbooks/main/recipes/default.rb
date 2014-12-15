232abcc1fc0f032578825e88ef47718eea07dd81
Antônio Carlos <acarlossw@gmail.com>
2014-12-15 15:08:20 -0200

    Adicionado link simbólico entre vagrant e o apach2

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -33,6 +33,13 @@ end
 execute "curl -sS https://getcomposer.org/installer | php"
 execute "mv composer.phar /usr/local/bin/composer"
 
+#Create a symbolic link 
+#form /vagrant (fold shared whit guest) to /var/www/resap (Apache2)
+execute "if [ ! -L /var/www ]; then
+          sudo rm -rf /var/www
+          sudo ln -s /vagrant /var/www
+        fi"
+
 #Enable mod_rewrite of Apache2
 #@see http://jaydson.org/habilitar-mod_rewrite-no-apache/
 execute "sudo a2enmod rewrite"
